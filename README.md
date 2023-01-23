31d7e90ff4c6c3d84f6897277678c92b6997b622
vanessa <vanessacamahuali28@gmail.com>
2023-01-23 09:46:49 -0300

    "estilizando"

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # desafio-teorema-pitagoras
+
 Este projeto é um desafio feito para testar uma calculadora que será capaz de calcular a relação entre os lados de um triângulo retângulo, conhecido popularmente como Teorema de Pitágoras.
+
+
---
+++ index.js
@@ -0,0 +1,31 @@
+function calculaTeorema(){
+    document.getElementById("resultado").style.display = 'flex';
+    const catetoA = document.getElementById("catetoA").value;
+    const catetoB = document.getElementById("catetoB").value;
+    const hipotenusa = document.getElementById("hipotenusa").value;       
+
+    let somaQuadradoCatetos = (catetoA*catetoA) + (catetoB*catetoB);
+
+    if(hipotenusa!="" && catetoA!="" && catetoB!=""){
+
+        if((hipotenusa*hipotenusa) === somaQuadradoCatetos){            
+            document.getElementById("retorno").innerHTML = "É um triângulo retângulo!";
+            document.getElementById("resposta").innerHTML = "Resolução: <br>" + hipotenusa + "² = " +catetoA + "² + " + catetoB + "² <br>" + hipotenusa*hipotenusa + " = " + (catetoA*catetoA) + " + " + (catetoB*catetoB) + "<br>" + hipotenusa*hipotenusa + " = " + somaQuadradoCatetos;
+        }else{
+            document.getElementById("retorno").innerHTML = "Não é um triângulo retângulo!";
+            document.getElementById("resposta").innerHTML = "Resolução: <br>" + hipotenusa + "² = " +catetoA + "² + " + catetoB + "² <br>" + hipotenusa*hipotenusa + " = " + (catetoA*catetoA) + " + " + (catetoB*catetoB) + "<br>" + hipotenusa*hipotenusa + " ≠ " + somaQuadradoCatetos;
+        }
+    }else{
+        alert("Há medidas inválidas, revise e tente novamente!");
+    }
+};
+
+
+function apagarCampos(){
+    document.getElementById("catetoA").value = ""; 
+    document.getElementById("catetoB").value = ""; 
+    document.getElementById("hipotenusa").value = ""; 
+    document.getElementById("retorno").innerHTML = "";
+    document.getElementById("resposta").innerHTML = "";
+    document.getElementById("resultado").style.display = 'none';
+}
